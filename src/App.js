c0d252bb869882d7348dcdaee6655892e6f75de3
Mario Lazzari <mario.lazzari@gmail.com>
2021-09-23 21:41:51 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -5,15 +5,7 @@ import Card from "./components/Card";
 import { Container } from "./components/styles/Container.styled";
 import GlobalStyles from "./components/styles/Global";
 import content from "./content";
-
-const theme = {
-  colors: {
-    header: "#ebfbff",
-    body: "#fff",
-    footer: "#003333",
-  },
-  mobile: "768px",
-};
+import theme from "./theme";
 
 function App() {
   return (
